3dc6f5b2ed566907db1aac50c28a3dd33d423988
Rasgrin <Stefanrogic@protonmail.com>
2022-08-26 23:45:36 +0200

    nvc

---
+++ src/ts/views/traficLightView.ts
@@ -0,0 +1,7 @@
+class TraficLight {
+  _parrentElement = document.querySelector('.tlight__container');
+  _errorMessage = "We can't get a number at the momment.";
+  _message = '';
+}
+
+export default new TraficLight();
